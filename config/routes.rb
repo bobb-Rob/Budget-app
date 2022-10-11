368aaab574a1cc7f3ccca9aaf2e62b8e8cd6f849
Robertson Arthur <robertsonakpan@gmail.com>
2022-10-11 16:08:49 +0100

    Add all controllers and their actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,23 @@
 Rails.application.routes.draw do
+  get 'categories/new'
+  get 'expenses/new'
+  get 'categories/index'
+  get 'categories/show'
+  get 'categories/create'
+  get 'categories/edit'
+  get 'categories/destroy'
+  get 'expenses/index'
+  get 'expenses/show'
+  get 'expenses/create'
+  get 'expenses/edit'
+  get 'expenses/destroy'
   
+  resources :users do
+    resources :categories do
+      resources :expenses
+    end
+  end
+
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
