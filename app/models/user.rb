8685f2d94a63740b4e9827e2b4335b62c08af72f
Robertson Arthur <robertsonakpan@gmail.com>
2022-10-15 00:52:27 +0100

    Complete unit test and integration test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,9 @@
 class User < ApplicationRecord
-  has_many :expenses
-  has_many :categories
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :expenses, dependent: :destroy
+  has_many :categories, dependent: :destroy  
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
-
+  
   validates :name, :email, presence: true
 end
